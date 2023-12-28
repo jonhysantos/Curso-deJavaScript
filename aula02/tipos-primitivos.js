dd7010bd45b33780171542cd8eb5875be3641ceb
Jonhy Santos <Jonhy1905@hotmail.com>
2023-12-27 23:20:47 -0300

    aula02

---
+++ aula02/tipos-primitivos.js
@@ -0,0 +1,55 @@
+// string
+// "letras entre aspas símples ou dupla"
+console.log('jonhy apertar f8 para executar')
+
+// Boolean
+// aceita os valores true e false
+console.log(true) // f8 para exec
+console.log(false)
+
+// Number 
+// aceita números
+console.log(55) // f8 pra exec
+
+// BigInt 
+// Números inteiros
+console.log(Number.MAX_SAFE_INTEGER) // quantidade suportada em js
+console.log(988888888888888 * 55555555555555) //sem big
+// com o BigInt 
+console.log(88888888888888888n * 451111111111111111n)
+
+// Variáveis
+let nome = "Jonhy"
+let idade = 35
+let peso = 82 
+console.log(nome,idade,peso) // usar let ou const
+console.log(`Seu nome é ${nome} anos você tem ${idade}, e pesa ${peso} kg`)
+const endereço = 'av altantica' // não muda imutavel
+console.log(endereço)
+
+//+ - * / %   // Aritméticos
+console.log(9 + 1)
+console.log(8-2)
+console.log(8 * 4)
+console.log(8 / 4)
+
+// Agrupamento ()
+console.log(2 * (8 / 4))
+
+// atribuição = += *= != %=
+
+// Lógico ==  ===  != !== >  >=  < <= comparaçoes
+// ==  Se são do mesmo valor === se são do mesmo tipo
+console.log('jonhy ' == 'jonhy ')
+console.log('jonhy ' != 'jonhy ')
+console.log('jonhy ' === 'jonhy ')
+
+// Incremento ++  --  // aumneta 1 e diminui 1 
+let n = 10 
+console.log(n ++)
+console.log(n --)
+
+
+
+
+
